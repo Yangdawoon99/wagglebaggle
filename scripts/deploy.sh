0d3132cb11e3e6d491bb9283af3feac6e7c2d6da
Yangdawoon99 <86522984+Yangdawoon99@users.noreply.github.com>
2024-05-19 15:44:25 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# deploy.sh
 
+# Install dependencies if necessary
+# sudo yum install -y httpd php php-mysql
+
+# Copy files to the web server root directory
+# Assuming CodeDeploy has copied the files to /var/www/html already
+
+# Restart Apache to apply changes
+sudo service httpd restart
