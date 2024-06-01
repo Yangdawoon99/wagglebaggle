8479d940e5318b3a3f99d0ee6214e007d5fdeda5
Yangdawoon99 <86522984+Yangdawoon99@users.noreply.github.com>
2024-06-01 11:10:39 +0900

    Create phpinfo.php

---
+++ phpinfo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcom Page</title>
+</head>
+<body>
+    <h1>Welcom App Server CI/CD!</h1>
+    <h1>RegionAz($RZAZ) : Instance ID($IID) : Private IP($LIP) : ALB Web Server</h1>
+    <img src="/IMG/white.png" alt="White Image">
+</body>
+</html>
+
+<?php
+// 파일을 /var/www/html/phpinfo.php로 저장
+phpinfo();
+?>
